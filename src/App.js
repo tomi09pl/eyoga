4ab892da32413157bc6a23ee4fe4e48b2de61170
tomi09pl <tomi09pl@gmail.com>
2020-03-05 18:47:08 +0000

    init repo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './scss/App.scss';
+
+
+const Test1 = () => (
+      <div>
+        <input type="text" placeholder="wpisuj tu text..."/>
+        <button>wciskaj buttona!</button>
+          <div>a to jest moj tekst ktory bede sobie tutaj pisac!</div>
+          <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab at debitis dicta dolor dolorum eius fugit illo inventore libero minima minus nostrum quas, quasi quibusdam quo quos recusandae repellat suscipit.</div>
+      </div>
+  );
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+          <Test1 />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
