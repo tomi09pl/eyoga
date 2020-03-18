13c46babd5dbf737da8fa4e1aad765b9b168ad2b
tomi09pl <tomi09pl@gmail.com>
2020-03-18 22:19:02 +0000

    Wed

--- src/components/Main_Section4_WhatIs.js
+++ src/components/Main_Section4_WhatIs.js
@@ -1,13 +1,44 @@
 import React from 'react';
+import Collapsible from 'react-collapsible';
 
 const WhatIs = () => {
 
-    return(
+    return (
         <div className="section4-div">
-                <h3 className="section4-h3">WHAT IS EYES YOGA</h3>
-                <h1 className='section4-h1'>The Bates Method for Better Eyesight</h1>
-                <p className='section4-p'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda beatae consectetur excepturi, expedita laudantium maiores molestias officia sint tempore totam vitae voluptates! Adipisci asperiores beatae consectetur doloribus esse ipsum nam nihil placeat quae, quisquam, quo, quos velit! Aliquam aspernatur beatae commodi labore minus nam necessitatibus quo ut. Alias cupiditate harum iusto maiores recusandae. Dignissimos magnam maiores qui saepe suscipit tempore temporibus vero. Beatae blanditiis commodi consequatur debitis ducimus ea error exercitationem, harum incidunt, inventore maxime molestiae nam nesciunt nostrum porro quae quaerat quam quasi quia ratione reprehenderit sequi similique unde vero vitae voluptas, voluptate voluptatum. Beatae itaque laborum nostrum officiis.</p>
-                <a className='section4-a collapsible' href='#'>MORE...</a>
+            <h3 className="section4-h3">WHAT IS EYES YOGA</h3>
+            <h1 className='section4-h1'>The Bates Method for Better Eyesight</h1>
+            <div className='box1'>
+                <p className='section4-p'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda beatae
+                    consectetur excepturi, expedita laudantium maiores molestias officia sint tempore totam vitae
+                    voluptates! Adipisci asperiores beatae consectetur doloribus esse ipsum nam nihil placeat quae,
+                    quisquam, quo, quos velit! Aliquam aspernatur beatae commodi labore minus nam necessitatibus quo ut.
+                    Alias cupiditate harum iusto maiores recusandae. Dignissimos magnam maiores qui saepe suscipit
+                    tempore temporibus vero. Beatae blanditiis commodi consequatur debitis ducimus ea error
+                    exercitationem, harum incidunt, inventore maxime molestiae nam nesciunt nostrum porro quae quaerat
+                    quam quasi quia ratione reprehenderit sequi similique unde vero vitae voluptas, voluptate
+                    voluptatum. Beatae itaque laborum nostrum officiis.</p>
+                <div className='sectionImg'></div>
+            </div>
+            <Collapsible>
+                <p className='section4-p'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda beatae
+                    consectetur excepturi, expedita laudantium maiores molestias officia sint tempore totam vitae
+                    voluptates! Adipisci asperiores beatae consectetur doloribus esse ipsum nam nihil placeat quae,
+                    quisquam, quo, quos velit! Aliquam aspernatur beatae commodi labore minus nam necessitatibus quo ut.
+                    Alias cupiditate harum iusto maiores recusandae. Dignissimos magnam maiores qui saepe suscipit
+                    tempore temporibus vero. Beatae blanditiis commodi consequatur debitis ducimus ea error
+                    exercitationem, harum incidunt, inventore maxime molestiae nam nesciunt nostrum porro quae quaerat
+                    quam quasi quia ratione reprehenderit sequi similique unde vero vitae voluptas, voluptate
+                    voluptatum. Beatae itaque laborum nostrum officiis.</p>
+                <p className='section4-p'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda beatae
+                    consectetur excepturi, expedita laudantium maiores molestias officia sint tempore totam vitae
+                    voluptates! Adipisci asperiores beatae consectetur doloribus esse ipsum nam nihil placeat quae,
+                    quisquam, quo, quos velit! Aliquam aspernatur beatae commodi labore minus nam necessitatibus quo ut.
+                    Alias cupiditate harum iusto maiores recusandae. Dignissimos magnam maiores qui saepe suscipit
+                    tempore temporibus vero. Beatae blanditiis commodi consequatur debitis ducimus ea error
+                    exercitationem, harum incidunt, inventore maxime molestiae nam nesciunt nostrum porro quae quaerat
+                    quam quasi quia ratione reprehenderit sequi similique unde vero vitae voluptas, voluptate
+                    voluptatum. Beatae itaque laborum nostrum officiis.</p>
+            </Collapsible>
         </div>
     )
 };
